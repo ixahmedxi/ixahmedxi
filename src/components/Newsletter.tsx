ae3a841ddec30c1259deddbeadc1d08a21245f77
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2020-08-26 16:40:07 +0300

    adds newsletter section

---
+++ src/components/Newsletter.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { FiMail } from 'react-icons/fi'
+import './Newsletter.scss'
+
+export const Newsletter = () => {
+  return (
+    <div className='newsletter-wrapper'>
+      <div className='filter' />
+      <div className='content-wrapper'>
+        <div className='icon'>
+          <FiMail />
+        </div>
+        <h3>Subscribe to my newsletter!</h3>
+        <p>
+          Never miss any of my new projects or blog posts when I start that. I
+          will not spam, I promise :)
+        </p>
+        <a
+          href='https://ixahmedix.me'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='button'>
+          Subscribe now!
+        </a>
+      </div>
+      <div className='credit'>
+        <span>
+          Photo by{' '}
+          <a
+            href='https://unsplash.com/@_imkiran?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText'
+            target='_blank'
+            rel='noopener noreferrer'>
+            Sai Kiran Anagani
+          </a>{' '}
+          on{' '}
+          <a
+            href='https://unsplash.com/s/photos/coding?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText'
+            target='_blank'
+            rel='noopener noreferrer'>
+            Unsplash
+          </a>
+        </span>
+      </div>
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { About } from '../components/About'
 import { Landing } from '../components/Landing'
+import { Newsletter } from '../components/Newsletter'
 import { Projects } from '../components/Projects'
 import SEO from '../components/seo'
 import { Skills } from '../components/Skills'
@@ -13,6 +14,7 @@ const index = () => {
       <About />
       <Skills />
       <Projects />
+      <Newsletter />
     </div>
   )
 }
