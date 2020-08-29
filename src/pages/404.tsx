eccd3e0f4c0c287df5156095fd601da333149f79
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2020-08-29 22:14:15 +0300

    404 page button

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'gatsby'
 import React from 'react'
 import illustration from '../assets/images/illustration.svg'
 import './404.scss'
@@ -10,10 +11,17 @@ const NotFound = ({ data }: any) => {
           <img src={illustration} alt='Illustration' />
         </div>
         <div className='right-side'>
-          <h1>
-            Oops! ... <br /> 404 Not Found
-          </h1>
-          <h3>It looks like this page does not exist...the sadness</h3>
+          <div className='inner-wrapper'>
+            <h1>
+              Oops! ... <br /> 404 Not Found
+            </h1>
+            <h3>It looks like this page does not exist...the sadness</h3>
+            <div className='button-wrapper'>
+              <Link to='/' className='button'>
+                Go to home page
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
