69d6ff2f43f15540c690b349ed4624c66d0455aa
Ahmed Tarek <ahmedtarek3214@gmail.com>
2020-09-25 01:30:13 +0100

    Adds styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react'
+import styled from 'styled-components'
 import SEO from '../components/seo'
+
+const Text = styled('h1')`
+  color: dodgerblue;
+`
 
 const IndexPage = () => {
   return (
     <div>
       <SEO title='Home' />
       <div>
-        <h1>Hello World</h1>
+        <Text>Hello World</Text>
       </div>
     </div>
   )
