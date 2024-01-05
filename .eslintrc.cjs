2efb16251cab3bb5023c35f0e249ae2af9e80e90
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-01-05 05:15:32 +0000

    feat: eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,65 @@
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  ignorePatterns: ['styled-system'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'plugin:eslint-comments/recommended',
+    'plugin:regexp/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:astro/jsx-a11y-strict',
+    'plugin:astro/recommended',
+    'prettier',
+  ],
+  rules: {
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array-simple',
+        readonly: 'array-simple',
+      },
+    ],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: { attributes: false },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['*.astro'],
+      processor: 'astro/client-side-ts',
+      parser: 'astro-eslint-parser',
+      extends: [],
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+        sourceType: 'module',
+      },
+      rules: {},
+    },
+    {
+      files: ['*.d.ts'],
+      rules: {
+        '@typescript-eslint/triple-slash-reference': 'off',
+      },
+    },
+  ],
+};
+
+module.exports = config;
