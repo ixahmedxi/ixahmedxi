6a13b2785481fc9867eb356d53e1011ff874a105
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-01-05 03:53:49 +0000

    feat: dark mode

--- panda.config.ts
+++ panda.config.ts
@@ -1,11 +1,33 @@
 import { defineConfig } from "@pandacss/dev";
+import radixColorsPreset from "pandacss-preset-radix-colors";
 
 export default defineConfig({
   preflight: true,
   include: ["./src/**/*.{js,jsx,ts,tsx,astro}"],
+  presets: [
+    radixColorsPreset({
+      autoP3: true,
+      darkMode: true,
+    }),
+    "@pandacss/preset-panda",
+  ],
+  conditions: {
+    dark: " &.dark, .dark &",
+    light: " &.light, .light &",
+    osDark: "@media (prefers-color-scheme: dark)",
+    osLight: "@media (prefers-color-scheme: light)",
+  },
   exclude: [],
   theme: {
     extend: {},
   },
   outdir: "styled-system",
+  globalCss: {
+    extend: {
+      body: {
+        backgroundColor: "gray.1",
+        color: "gray.11",
+      },
+    },
+  },
 });
